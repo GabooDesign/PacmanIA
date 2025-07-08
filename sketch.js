30c99a01fc08477b09c4fbbb36560af78888e5d6
Gaboo <94022396+GabooDesign@users.noreply.github.com>
2025-07-08 09:25:44 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,216 +1,88 @@
-/*
- * This work has been created using p5js.org 1.9.4 and ml5js.org 1.2.1 
- * Using the template of Bodypose as base of this project
- * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next gen/blob/main/LICENSE.md
- *
- * This project has been created by GabooDesign for the class of "Proyecto V" by Jenny Abud.
- * If you wanna use it for any pourposes please let me know ;)
- */
+// This project is debug only!!!
 
-// Webcam & ml5.js variables
 let video;
-let bodyPose;
-let poses = [];
-let connections;
+let font;
 
-// Pre-Loads
-let imgplayer; // Preloads the image of the pacman player
-let imgplayernose; // Preloads the image of player nose
-let numPlayers = 0; // Temporal ------------------------------------------
-let font; // Preloads the font for the game
+// Change between into UI
+let gamePlay = false; // UI during runtime
+let gameOver = false; // UI Game Over
+let standBy = true; // UI StandBy
 
-// Gamestates and UI
-    // images
-let ui_gameplay; // user interface for the gameplay
-let ui_death; // user interface for dead screen
-
-let gameState = "playing"; // gamestates = "gameOver", "playing", "standBy"
-let countdown = 9; // Countdown to start the game again as a loop
-let gameOverStartTime = 0; //Millis to count back
-
-// *Player*
-let playerDetected = false; // Detects if there is a player in the Capture
-let playCredits = 0; // Time that the player can still playing
-let lastUpdate = 0; // Last time update
-let highScore = 0; // The most time that has been saved during Runtime
-let posX = 0; // Position in X of the nose of the player
-
-// *Enemies*
-let imgEnemies = []; // Preloads an array of gif enemies
-let enemies = [];
-
+// Preloads of images
+let ui_gameplay; // UI Background
+let ui_death; // UI death
 
 function preload() {
-  //----------------------------------------------------------------------- PRELOAD
-  // Preload the bodyPose model
-  bodyPose = ml5.bodyPose();
-
-  // Preload the image of the player
-  imgplayer = loadImage("sprites/player.gif");
-  imgplayernose = loadImage("sprites/playerNose.png");
-
   // Preload the font of the game
-  font = loadFont("fonts/8-bit-hud.ttf");
+  font = loadFont("8-bit-hud.ttf");
 
-  // Preload the user interface images
-  ui_gameplay = loadImage('sprites/ui_runtime.png')
-  ui_death = loadImage('sprites/ui_dead.png')
-  
-  // Preload the enemies array of gifs
-  imgEnemies = [
-    loadImage("sprites/enemies/enemie1.gif"),
-    loadImage("sprites/enemies/enemie2.gif"),
-    loadImage("sprites/enemies/enemie3.gif"),
-    loadImage("sprites/enemies/enemie4.gif"),
-  ];
+  ui_gameplay = loadImage("ui_runtime.png");
+  ui_death = loadImage("ui_dead.png");
 }
 
-
 function setup() {
-  //----------------------------------------------------------------------- SETUP
   createCanvas(640, 480);
 
-  // Create the video and hide it
   video = createCapture(VIDEO);
   video.size(640, 480);
   video.hide();
-
-  // Start detecting poses in the webcam video
-  bodyPose.detectStart(video, gotPoses);
-  // Get the skeleton connection information
-  connections = bodyPose.getSkeleton();
-
-  // Create enemies
-  enemies = [
-    { x: random(width), y: random(-300, -50), speed: 2.2, img: imgEnemies[0] },
-    { x: random(width), y: random(-300, -50), speed: 2, img: imgEnemies[1] },
-    { x: random(width), y: random(-300, -50), speed: 3.1416, img: imgEnemies[2], },
-    { x: random(width), y: random(-300, -50), speed: 3.5, img: imgEnemies[3] }
-  ];
 }
 
-
 function draw() {
-  //------------------------------------------------------------------------ DRAW
   // Shows webcam
   image(video, 0, 0, width, height);
 
-  //Sets the HighScore if the PlayCredits are upper than the actual value
-  if (playCredits >= highScore) {
-    highScore = playCredits;
-  }
-
-    // Enemies logic  
-    if(gameState === 'playing'){
-      // Restart the enemies
-      for (let i = 0; i < enemies.length; i++) {
-      let e = enemies[i];
-      e.y += e.speed;
-
-      if (e.y > height + 40) {
-        playCredits++; // sumás crédito
-        e.y = random(-300, -50);
-        e.x = random(width);
-      }
-        if (
-          playerDetected &&
-          posX < e.x + 50 &&
-          posX + 50 > e.x &&
-          400 < e.y + 50 &&
-          400 + 50 > e.y
-        ) {
-          if (gameState !== "gameOver") {
-            gameState = "gameOver";
-            gameOverStart = millis();
-            playCredits = 0;
-          }
-        }
-
-      image(e.img, e.x, e.y, 50, 50);
-      }
-    
-    // image background  
+  // Shows UI while GamePlay
+  if (gamePlay === true) {
     image(ui_gameplay, 0, 0, width, height);
 
-    // texts
     fill(255);
     textSize(12);
     textFont(font);
     textAlign(LEFT, BOTTOM);
     stroke(0);
-    text("High Score: " + highScore, 15, 470);
-    text("Your Score: " + playCredits, 440, 470);
-    }
-    if(gameState === 'gameOver'){
-      gameIsOver();
-      playCredits = 0;
-    }
-  
-  // ml5.js ia body detection
-  for (let i = 0; i < poses.length; i++) {
-    let pose = poses[i];
-    for (let j = 0; j < pose.keypoints.length; j++) {
-      let keypoint = pose.keypoints[0];
-
-      if (keypoint.confidence > 0.1) {
-        playerDetected = true; // Detects an human
-        image(imgplayernose, keypoint.x-25/2, keypoint.y-25/2, 25, 25);
-        posX = keypoint.x;
-        image(imgplayer, keypoint.x, 380, 50, 50);  
-        } else { // Si no detecta humanos...
-          gameState = 'standBy';
-        }
-        
-      }
-    }
-} // End function Draw
-
-function gameIsOver() {
-  let elapsed = int((millis() - gameOverStart) / 1000);
-  let remaining = countdown - elapsed;
-
-  // Muestra la pantalla de Game Over
-  image(ui_death, 0, 0, width, height);
-
-  // Texto principal
-  fill(255, 0, 0);
-  textSize(32);
-  textFont(font);
-  textAlign(CENTER, CENTER);
-  stroke(0);
-  strokeWeight(1);
-  text("Game Over !", width / 2, height / 2);
-  textStyle(BOLD);
-
-  // Puntajes
-  fill(255);
-  textSize(16);
-  text("High Score: " + highScore, width / 2, 180);
-  text("Your Score: " + playCredits, width / 2, 300);
-
-  // Countdown visual
-  textSize(10);
-  text("next game will begin in... " + remaining + " segs", 320, 470);
-
-  // Si se acaba el tiempo, reiniciar
-  if (remaining <= 0) {
-    resetGame();
+    text("High Score: " + "XX", 15, 470);
+    text("Your Score: " + "XX", 440, 470);
   }
-}
 
-function resetGame() {
-  // Reinicia el juego
-  gameState = "playing";
-  countdown = 9;
-  
-  for (let i = 0; i < enemies.length; i++) {
-    enemies[i].x = random(width);
-    enemies[i].y = random(-300, -50);
+  // Shows UI in the deadscreen
+  if (gameOver === true) {
+    // Background
+    image(ui_death, 0, 0, width, height);
+
+    // GAME OVER
+    fill(255, 0, 0);
+    textSize(32);
+    textFont(font);
+    textAlign(CENTER, CENTER);
+    stroke(0);
+    strokeWeight(1);
+    text("Game Over !", width / 2, height / 2);
+    textStyle(BOLD);
+    textAlign(CENTER);
+
+    // Shows Player scores and high score saved during runtime
+    fill(255);
+    textSize(16);
+    textFont(font);
+    textAlign(CENTER, CENTER);
+    stroke(5);
+    strokeWeight(4);
+    text("High Score: " + "XX", width / 2, 180);
+    text("Your Score: " + "XX", width / 2, 300);
+
+    // Timer to next game
+    fill(255);
+    textSize(10);
+    textFont(font);
+    textAlign(CENTER, BOTTOM);
+    stroke(0);
+    text("next game will begin in... " + "9" + " segs", 320, 470);
   }
-}
 
-function paused(){
-      // Background
+    // Shows UI in the deadscreen
+  if (standBy === true) {
+    // Background
     image(ui_death, 0, 0, width, height);
 
     // Title
@@ -231,33 +103,24 @@ function paused(){
     textAlign(CENTER, CENTER);
     stroke(5);
     strokeWeight(5);
-    text("High Score: " + highScore, width / 2, 300);
+    text("High Score: " + "25", width / 2, 300); // replace with highScore
     text("IA en los videojuegos", width / 2, 200);
 
-    // Instructions
+    // Timer to next game
     fill(255);
-    textSize(14);
-    textFont(font);
-    textAlign(CENTER, BOTTOM);
-    stroke(0);
-    text("Ponte frente a la webcam para inciar.", 320, 430);
-    
-    // Credits
-    fill(255);
-    textSize(10);
+    textSize(12);
     textFont(font);
     textAlign(CENTER, BOTTOM);
     stroke(0);
     text("by GabooDesign", 320, 470);
-    
-    // Image
-    image(player, width / 2 + 210, height / 2 - 25, 50, 50);
+  
+  }
+  
 }
 
-
-// Callback function for when bodyPose outputs data
-function gotPoses(results) {
-  //-----------------------------------------------------------------------
-  // Save the output to the poses variable
-  poses = results;
+// Save Screenshot
+function keyPressed() {
+  if (key === "s") {
+    saveCanvas();
+  }
 }
